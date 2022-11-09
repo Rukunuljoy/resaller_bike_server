46f2ed3590c446cc150fa2e1b268c9c77b2d690b
Rukunuljoy <rukunuljoypub58@gmail.com>
2022-11-09 07:59:53 +0600

    third commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config()
@@ -13,11 +13,30 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hvxqvqc.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function running(){
+    try{
+        const serviceCollection = client.db('foodService').collection('services');
+
+        app.get('/services',async(req,res)=>{
+            const query = {};
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray();
+            res.send(services);
+
+            app.get('/services/:id', async(req,res)=>{
+                const id = req.params.id;
+                const query = {_id:ObjectId(id)};
+                const service = await serviceCollection.findOne(query)
+                res.send(service)
+            })
+        })
+    }
+    finally{
+
+    }
+}
+running().catch(err=>console.error(err));
 
 
 app.get('/',(req,res) => {
