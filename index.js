89137e6a6aebaa6658de799a51163ae5d35d30ed
Rukunuljoy <rukunuljoypub58@gmail.com>
2022-11-09 07:29:42 +0600

    mongo setup

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+require('dotenv').config()
+
+app.use(cors())
+app.use(express.json())
+
+// oAAcsEqgyvnSmBOI
+// foodService
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hvxqvqc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+app.get('/',(req,res) => {
+    res.send('Food review')
+})
+app.listen(port,() =>{
+    console.log(`food service listening on port ${port}`)
+})
